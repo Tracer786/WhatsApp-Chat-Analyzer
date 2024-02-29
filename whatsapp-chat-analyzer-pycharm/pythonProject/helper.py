78be94f2ca735415a55f02a3ca7c5d8960592774
Tracer786 <msingh6_be20@thapar.edu>
2024-02-29 14:04:02 +0530

    Count Messages,Words (Pycharm)
    will write the code in the file helper.py for the count of messages and words in the chat

--- whatsapp-chat-analyzer-pycharm/pythonProject/helper.py
+++ whatsapp-chat-analyzer-pycharm/pythonProject/helper.py
@@ -1,7 +1,15 @@
 def fetch_stats(selected_user, df):
 
     if selected_user == 'Overall':
-        return df.shape[0]  # returning the total number of messages
+        # 1. Fetch number of messages
+        num_messages = df.shape[0]  # returning the total number of messages
+
+        # 2. Fetch number of words
+        words = []
+        for message in df['message']:
+            words.extend(message.split())
+
+        return num_messages, words
 
     # else we need to return the count of messages for the particular user
     # for that we need to use the concept of masking
